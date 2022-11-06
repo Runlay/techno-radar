b1ac79b88f7906501d25a3cb6e2c33ace96b2a8d
Max Finkenbeiner <48648552+Runlay@users.noreply.github.com>
2022-11-06 12:48:41 +0100

    init

---
+++ app.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    //do work
+
+    // Get the video
+    var video = document.getElementById("start-sq");
+
+    // Get the button
+    var btn = document.getElementById("myBtn");
+
+    var text = document.getElementById("text")
+
+    // Pause and play the video, and change the button text
+    function startPlayer() {
+
+
+
+    }
+
+    //document.getElementById("bar1").style.animation = "audiomover" + (Math.random() * 100) + "s infinite";
+
+    // window.addEventListener("scroll", fadeIn);
+
+
+    // function fadeIn() {
+    //     var textPosition = text.getBoundingClientRect().top;
+    //     var windowHeight = window.innerHeight;
+    //     if (windowHeight > textPosition) {
+    //         console.log("remove")
+    //         text.classList.remove("push-left");
+    //         // image.classList.remove("push-right");
+    //     } else {
+    //         text.classList.add("push-left");
+    //         // image.classList.add("push-right");
+    //     }
+    // }
+
+    const inViewport = (entries, observer) => {
+        entries.forEach(entry => {
+            entry.target.classList.toggle("is-inViewport", entry.isIntersecting);
+        });
+    };
+
+    const Obs = new IntersectionObserver(inViewport);
+    const obsOptions = {}; //See: https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API#Intersection_observer_options
+
+    // Attach observer to every [data-inviewport] element:
+    const ELs_inViewport = document.querySelectorAll('[data-inviewport]');
+    ELs_inViewport.forEach(EL => {
+        Obs.observe(EL, obsOptions);
+    });
+});
